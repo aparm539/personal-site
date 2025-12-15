d13d6d64db34f66166a2b89aa38b1c03db1179a8
Sunny <aparm539@mtroyal.ca>
2025-12-15 12:08:41 -0700

    Add more metadata, jsonld, and sitemap.xml for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,21 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sunny's Portfolio",
-  description: "Sunny Parmar's personal site.",
+  title: 'Sunny Parmar | Full-Stack Developer',
+  description: 'The personal portfolio of Sunny Parmar, a full-stack developer.',
+  keywords: ['Sunny Parmar', 'Full-Stack Developer', 'Portfolio', 'Web Developer'],
+  authors: [{ name: 'Sunny Parmar' }],
+  creator: 'Sunny Parmar',
   icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👋</text></svg>",
+  openGraph: {
+    title: 'Sunny Parmar - Personal Site',
+    description: 'Portfolio of Sunny Parmar, a full-stack developer.',
+    url: 'https://sunnyparmar.com',
+    siteName: 'Sunny Parmar',
+    locale: 'en_US',
+    type: 'website',
+  },
 };
-
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,25 @@ import { Linkedin } from 'lucide-react';
 import SocialIconContainer from './components/social-icon-container';
 import GithubGraph from './components/github-graph';
 import StravaActivities from './components/strava-activities';
+import Script from 'next/script';
 
 export default async function HomePage() {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Sunny Parmar',
+    url: 'https://sunnyparmar.com',
+    sameAs: [
+      'https://www.linkedin.com/in/sunny-parmar-yyc',
+      'https://github.com/aparm539',
+      'https://www.instagram.com/sunnyparmar22'
+    ],
+    jobTitle: 'Software Engineer',
+    worksFor: {
+      '@type': 'Organization',
+      name: 'Freelance'
+    }
+  }
   return (
     <div className="mx-auto max-w-4xl">
       <div className="flex flex-col mt-2 px-6 py-12 md:px-12 md:py-16">
@@ -35,6 +52,13 @@ export default async function HomePage() {
         
         <StravaActivities />
       </div>
+      <Script
+  id="json-ld"
+  type="application/ld+json"
+  strategy="beforeInteractive"
+>
+  {JSON.stringify(jsonLd)}
+</Script>
  
   </div>
   );
